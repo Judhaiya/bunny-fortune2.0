8043563567ae0a548bc018c07e42bb1aa981fe00
jeyaudhaiya <udiaaral11@gmail.com>
2023-07-05 21:28:56 +0530

    just changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import FortunecardItems from "./components/FortunecardItems";
 import './App.css';
 import bunny from "./components/bunny.png"
@@ -6,57 +6,57 @@ import bunny from "./components/bunny.png"
 
 
 const App = () => {
-const [fortuneCards, setFortuneCards]=useState(FortuneContent)
+    const [fortuneCards, setFortuneCards] = useState(FortuneContent)
 
 
-    return(
+    return (
         <>
-        <div className="bunny-title">
-            <h1 className="title">Bunny the fortune teller </h1>
-            
-        </div>
-        <div className="card-wrapper"> <FortunecardItems fortuneCards={fortuneCards} /></div>
-        <p className="credits"><q>Image by Nara Kim from pixabay</q></p>
+            <div className="bunny-title l">
+                <h1 className="title">Bunny the fortune teller </h1>
+
+            </div>
+            <div className="card-wrapper"> <FortunecardItems fortuneCards={fortuneCards} /></div>
+            <p className="credits"><q>Image by Nara Kim from pixabay</q></p>
         </>
     )
 }
 
-const FortuneContent =[
+const FortuneContent = [
     {
-        id:1,
-       image: bunny,
-   
-       
-       fortuneLines:"It\'s okay to talk to strangers " 
-        
-        
+        id: 1,
+        image: bunny,
+
+
+        fortuneLines: "It\'s okay to talk to strangers "
+
+
     },
     {
-        id:2,
+        id: 2,
         image: bunny,
-        fortuneLines:'Mistakes happen, turn them into a opportunity',
-       
+        fortuneLines: 'Mistakes happen, turn them into a opportunity',
+
     },
     {
-        id:3,
+        id: 3,
         image: bunny,
-        fortuneLines:'There is a great love in your future',
-        
+        fortuneLines: 'There is a great love in your future',
+
     },
     {
-        id:4,
+        id: 4,
         image: bunny,
-        fortuneLines:'You won\'t know until you try',
+        fortuneLines: 'You won\'t know until you try',
     },
     {
-        id:5,
+        id: 5,
         image: bunny,
-        fortuneLines:'sharing is caring',
+        fortuneLines: 'sharing is caring',
     },
     {
-        id:6,
+        id: 6,
         image: bunny,
-        fortuneLines:'You can make your own happiness',
+        fortuneLines: 'You can make your own happiness',
     },
 ]
 export default App;
